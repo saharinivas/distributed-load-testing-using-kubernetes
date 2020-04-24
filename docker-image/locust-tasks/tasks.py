2fb6a1fe93767faddf4ea6dbdda3644fe00eecf6
Ravi Sharma <rs270297@gmail.com>
2020-04-25 00:54:44 +0530

    Update tasks.py

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -1,26 +1,63 @@
 from locust import HttpLocust, TaskSet, TaskSequence, seq_task
 from random import randrange
+import json
 
 
+#constants
+cookies = {}
+syllabus_data = {}
+answer_data = {}
+user_emails = []
 headers = {
-'Content-Type': 'application/json',
-'Cookie': 'PHPSESSID=gpsi2or2l71vfo4v6qmu8gqet0; PHPSESSID=gpsi2or2l71vfo4v6qmu8gqet0; AWSALB=KbJK/T8knVZNFpTcX25QRdRnfxq+l9pYzy0TiVql1sDAg9FKnSOhE4NwHjqbAmyUD05Czlzf4OJBZnYrsUsx2P+AekuTVrrhBxvYlG0BycIDzpiZ8sMM3isblodU; AWSALBCORS=KbJK/T8knVZNFpTcX25QRdRnfxq+l9pYzy0TiVql1sDAg9FKnSOhE4NwHjqbAmyUD05Czlzf4OJBZnYrsUsx2P+AekuTVrrhBxvYlG0BycIDzpiZ8sMM3isblodU'
+    'accept': 'application/json, text/plain, */*',
+    'content-type': 'application/json;charset=UTF-8',
 }
 
-user_emails= []
+#/locust-tasks/
 
-try:
-    with open('/locust-tasks/bulk_users.txt','r') as file:
-        user_emails = file.read().split(",")
-except:
-    pass
+#functions 
+with open('user_emails.txt','r') as file:
+    user_emails = file.read().split(",")
 
 
 class MyTaskSequence(TaskSequence):
+
     @seq_task(1)
-    def student_login(self):
-    	data = {"partner_id": "educationgoa", "email": user_emails[randrange(50000)]} 
-    	response = self.client.post(url="/apis/student_login.php",data=data,auth=None,headers=headers)
+    def login_funtoot(self):
+        
+        user_email = user_emails[randrange(len(user_emails)-1)]
+        
+        login_data = {
+            "UserName": user_email,
+            "Password": 'funtoot'
+        }
+        
+        response = self.client.post(url="/api/Account/Login", data=json.dumps(login_data), auth=None,headers=headers)
+        cookies['.ASPXAUTH'] = response.cookies['.ASPXAUTH']
+        cookies['LogId'] = response.cookies['LogId']
+        
+    @seq_task(2)
+    def profile_funtoot(self):
+        response = self.client.get(url="/api/Account/GetProfile", auth=None,headers=headers, cookies=cookies)
+
+    @seq_task(3)
+    def get_usage_funtoot(self):
+        response = self.client.get(url="/api/Student/GetUsage", auth=None,headers=headers, cookies=cookies)
+
+    @seq_task(4)
+    def get_syllabus_funtoot(self):
+        response = self.client.get(url="/api/Syllabus/Get", auth=None,headers=headers, cookies=cookies)
+        syllabus_data['data'] = response.json()['Content'][0]['Children'][0]['Children'][0]
+
+    @seq_task(5)
+    def get_problem_funtoot(self):
+        response = self.client.post(url="/api/Problem/Generate", data=json.dumps(syllabus_data['data']), auth=None,headers=headers, cookies=cookies)
+        answer_data['data'] = response.json()
+
+    @seq_task(6)
+    def get_solution_funtoot(self):
+        response = self.client.post(url="/api/Problem/Evaluate", data=json.dumps(answer_data['data']), auth=None,headers=headers, cookies=cookies)
+        
 
 
 class WebsiteTest(HttpLocust):
