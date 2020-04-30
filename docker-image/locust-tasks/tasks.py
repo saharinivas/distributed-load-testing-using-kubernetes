50a46406eb8727d42f24d007268f47a602b3a61d
Ravi sharma <ravi@Ravis-MacBook-Pro.local>
2020-04-30 11:16:17 +0530

    Akash

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -1,55 +1,69 @@
 from locust import HttpLocust, TaskSet, TaskSequence, seq_task
 from random import randrange
 import json
+import csv
 
 
-#constants
-cookies = {}
-syllabus_data = {}
-answer_data = {}
-user_emails = []
+
+
 headers = {
-    'accept': 'application/json, text/plain, */*',
-    'content-type': 'application/json;charset=UTF-8',
+    'embibe-token': ''
 }
 
-#/locust-tasks/
 
-#functions 
-with open('/locust-tasks/user_emails.txt','r') as file:
-    user_emails = file.read().split(",")
+login_response = {
+    "user_id":""
+}
 
+mocktest_session = {
+    "mocktest_session": {
+        "xpath": "/jee-main/chapterwise-test/chemistry/haloarenes",
+        "goal_code": "engineering",
+        "password": None,
+        "source": "offline_app:Aakash--SandwichApp"
+    }
+}
 
-class MyTaskSequence(TaskSequence):
+submit_sync = {
+    
+}
+
+email_password = []
+
+with open('/locust-tasks/emails_password.csv','r') as file:
+    rows = csv.reader(file)
+    email_passwords = [row for row in rows]
+
+  
+
+class UserBehavior(TaskSequence):
 
     @seq_task(1)
-    def login_funtoot(self):
-        
-        user_email = user_emails[randrange(len(user_emails)-1)]
-        
-        login_data = {
-            "UserName": user_email,
-            "Password": 'funtoot'
+    def login(self):
+        data = {
+            'user[login]': email_passwords[randrange(len(email_passwords)-1)][1],
+            'user[password]': email_passwords[randrange(len(email_passwords)-1)][2],
+            'app_id': 'Aakash--SandwichApp',
+            'pack_type': 'PACK-AAKASH'
         }
-        
-        response = self.client.post(url="/api/Account/Login", data=json.dumps(login_data), auth=None,headers=headers)
-        cookies['.ASPXAUTH'] = response.cookies['.ASPXAUTH']
-        cookies['LogId'] = response.cookies['LogId']
+        response = self.client.post(url = "/mobile/sandwich_app/login", headers=headers, data=data)
+        assert response.json()['success'] == True
+        login_response["user_id"] = response.json().get("user_id","")
+        headers["embibe-token"] = response.headers["embibe-token"]
+
 
     @seq_task(2)
-    def get_syllabus_funtoot(self):
-        response = self.client.get(url="/api/Syllabus/Get", auth=None,headers=headers, cookies=cookies)
-        syllabus_data['data'] = response.json()['Content'][0]['Children'][0]['Children'][0]
-
+    def fetch(self):
+        response = self.client.get(url = f"https://preprodms.embibe.com/content_ms_lt/v1/mobile-application/fetch-app-data?app_id=Aakash--SandwichApp&app_version=4.0.0&user_id={login_response['user_id']}",
+        name="https://preprodms.embibe.com/fetch", headers=headers)
+        assert response.json()['success'] == True
+    
     @seq_task(3)
-    def get_problem_funtoot(self):
-        response = self.client.post(url="/api/Problem/Generate", data=json.dumps(syllabus_data['data']), auth=None,headers=headers, cookies=cookies)
-        answer_data['data'] = response.json()
-
-    @seq_task(4)
-    def get_solution_funtoot(self):
-        response = self.client.post(url="/api/Problem/Evaluate", data=json.dumps(answer_data['data']), auth=None,headers=headers, cookies=cookies)
+    def istime(self):
+        response = self.client.get(url = f"https://preprodms.embibe.com/content_ms_lt/v1/mocktest-schedules/is-time?xpath={mocktest_session['mocktest_session']['xpath']}&institute_id={856}&branch_id=null&group_id=1004&batch_id=null&key=starts_at&app_id=Aakash--SandwichApp&app_version=4.0.0&user_id={login_response['user_id']}",
+        name="https://preprodms.embibe.com/is_time", headers=headers)
+        assert response.json()['success'] == True
 
 
-class WebsiteTest(HttpLocust):
-    task_set = MyTaskSequence
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
